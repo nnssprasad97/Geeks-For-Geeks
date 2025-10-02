2416cb90e3e53d1ca0dcc6630ce44adda024a0f7
Neelam naga satya sri prasad <157829337+nnssprasad97@users.noreply.github.com>
2025-10-02 23:53:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome/palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int n) {
+        if(n<0)return false;
+        int original=n;
+        int reversed=0;
+        while(n>0){
+            int digit=n%10;
+            reversed=reversed*10+digit;
+            n/=10;
+        }
+        return original==reversed;
+    }
+}
